9ff42939cbe90a26f3c44dcf4f229b4cb45f0f02
Gabriel Hartmann de Azeredo <gabriel.hazeredo@gmail.com>
2023-02-24 10:55:27 -0300

    first commit

---
+++ nlp_test_neoway/config.py
@@ -0,0 +1,8 @@
+from os import path
+
+import nlp_test_neoway
+
+base_path = path.dirname(path.dirname(nlp_test_neoway.__file__))
+workspace_path = path.join(base_path, 'workspace')
+data_path = path.join(workspace_path, 'data')
+models_path = path.join(workspace_path, 'models')
